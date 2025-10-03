eb656428d493ca84c579381b2d3fb56c6a528128
Caio <caio.vghedini@hotmail.com>
2025-10-02 21:05:07 -0300

    Primeiro commit python

---
+++ hello.py
@@ -0,0 +1,36 @@
+def main(): 
+    print("Olá Github")
+    print("Meu primeiro versionamento")
+    
+if __name__ == "__main__":
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
